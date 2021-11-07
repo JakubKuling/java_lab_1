4ef9b26c618579cebfd5cca46e107723bec55007
2a93 <wawer.j@gmail.com>
2021-11-07 15:56:52 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Dzień dobry");
+        System.out.println("jeden");
+        System.out.println("dwa");
+        System.out.println("jeden" + " jeden");
+        System.out.println("waf");
 
-    }
+        }
 }
