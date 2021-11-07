b498f902016e9505ddddec5b3f00c3a41fd0bc48
2a93 <wawer.j@gmail.com>
2021-11-07 17:06:27 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,24 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int zmiennaA = 9;
+        System.out.println("zmienna A =  " + zmiennaA);
+        int zmiennaC = 9;
+        System.out.println("zmienna C = " + zmiennaC);
+        int zmiennaH = 10214;
+        System.out.println("zmienna H = " + zmiennaH);
+        int jeden = 8912912;
+        System.out.println("zmienna jeden = " + jeden);
+        int dwa = 812412;
+        System.out.println("zmienna dwa = " + dwa);
+        int qwe = 1912;
+        System.out.println("zmienna qwe = " + qwe);
+        int vfr = 8412;
+        System.out.println("zmienna vfr = " + vfr);
+        int hej = 852;
+        System.out.println("zmienna jeden = " + hej);
+
+
 
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
@@ -16,6 +34,38 @@ public class Main {
         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
+        int liczbaA = 50;
+        int liczbaB = 10;
+
+            int dodawanie = liczbaA + liczbaB;
+            System.out.println("dodawanie = " + dodawanie);
+            int odejmowanie = liczbaA - liczbaB;
+            System.out.println("odejmowanie = " + odejmowanie);
+            int mnozenie = liczbaA * liczbaB;
+            System.out.println("mnozenie = " + mnozenie);
+            int dzielenie = liczbaA / liczbaB ;
+            System.out.println("dzielenie = " + dzielenie);
+            int modulo = liczbaA % liczbaB ;
+            System.out.println("modulo = " + modulo);
+
+        double liczbaX = 4.5;
+        double liczbaY = 5.2;
+
+            double dodawanieA = liczbaX + liczbaY ;
+            System.out.println("dodawanieA = " + dodawanieA);
+            double odejmowanieA = liczbaX - liczbaY;
+            System.out.println("odejmowanieA = " + odejmowanieA);
+            double mnozenieA = liczbaX * liczbaY;
+            System.out.println("mnozenieA = " + mnozenieA);
+            double dzielenieA = liczbaX / liczbaY;
+            System.out.println("dzielenieA = " + dzielenieA);
+            double moduloA = liczbaX % liczbaY;
+            System.out.println("moduloA = " + moduloA);
+
+
+
+
+
 
 
     }
